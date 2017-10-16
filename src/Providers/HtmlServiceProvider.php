158868f13d656873801115b6c53f5efe59c6e126
Shyam Achuthan <shyam@shyamachuthan.com>
2017-10-16 22:49:58 +0530

    Service provider updates
    Fixing bugs for compatibility with laravel collective html lib

--- src/Providers/HtmlServiceProvider.php
+++ src/Providers/HtmlServiceProvider.php
@@ -3,7 +3,7 @@
 namespace ShyamAchuthan\Laravel5Bootstrap3Form\Providers;
 
 use ShyamAchuthan\Laravel5Bootstrap3Form\Html\FormBuilder as FormBuilder;
-use Illuminate\Html\HtmlServiceProvider as IlluminateHtmlServiceProvider;
+use Collective\Html\HtmlServiceProvider as IlluminateHtmlServiceProvider;
 
 class HtmlServiceProvider extends IlluminateHtmlServiceProvider {
 
@@ -16,7 +16,7 @@ class HtmlServiceProvider extends IlluminateHtmlServiceProvider {
     {
         $this->app->singleton('form', function($app)
         {
-            $form = new FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+            $form = new FormBuilder($app['html'], $app['url'], $app['view'],$app['session.store']->getToken());
 
             return $form->setSessionStore($app['session.store']);
         });
